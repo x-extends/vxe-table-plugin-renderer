e446445fa61fa463b61e710ff7d62c6005b1dd13
xuliangzhan <xu_liangzhan@163.com>
2019-08-30 23:42:57 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ export interface VXETablePluginStatic {
 }
 
 /**
- * vxe-table renderer plugins for iview.
+ * 基于 vxe-table 表格的增强插件，提供一些常用的渲染器
  */
 declare var VXETablePluginRenderer: VXETablePluginStatic;
 
